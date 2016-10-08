84425d0d472b8acc59fc78674fb388041b36986f
pauljeffress <paul.jeffress@gmail.com>
2016-10-08 19:09:33 +1100

    Get PIR code working under Universal Node
    This was the first time I had used the PIRs in my Universal Node Code,
    so I had to tidy them up in general, let alone add the "send when it
    turns OFF" stuff.

--- checkPIRs.cpp
+++ checkPIRs.cpp
@@ -1,17 +1,30 @@
 //
 // ================ checkPIRs()
+// Check if any PIRs have a detection.
 //
+#include "a.h" // My global defines and extern variables to help multi file comilation.
+
+#ifdef PIR1 or PIR2
+
 void checkPIRs() {
+
+  #ifdef DEBUGPJx
+	  Serial.println("S: CheckPIRs");
+  #endif
 
   int pir1wait;
   int pir2wait;
   
-  #ifdef PIR1 or PIR2
+  #ifdef PIR1
     //PIRdelay; // give the grid time to stabilize for the PIR, otherwise false triggers occur after an RFM send/rcv (might need 2s)
     pir1wait = ((millis() - lastPIR1) < (PIRHOLDOFF * 1000)); // hold-off time for additional PIR1 messages 
+  #endif
+
+  #ifdef PIR2
+    //PIRdelay; // give the grid time to stabilize for the PIR, otherwise false triggers occur after an RFM send/rcv (might need 2s)
     pir2wait = ((millis() - lastPIR2) < (PIRHOLDOFF * 1000)); // hold-off time for additional PIR2 messages
   #endif
-  
+
   #ifdef PIR1
     curPIR1State = digitalRead(Pir1Input); // read PIR digital pin
     if (!pir1wait && (curPIR1State == 1)) 
@@ -19,7 +32,9 @@ void checkPIRs() {
       delay(5);
       lastPIR1 = millis(); // take timestamp
       send41 = true; // set PIR1 message flag
-      Serial.println("PIR1 detect");
+      #ifdef DEBUGPJ2
+        Serial.println("PIR1 detect");
+      #endif
       }
   #endif
 
@@ -30,8 +45,11 @@ void checkPIRs() {
       delay(5);
       lastPIR2 = millis(); // take timestamp
       send43 = true; // set PIR2 message flag
-      Serial.println("PIR2 detect");
+      #ifdef PJDEBUG2
+        Serial.println("PIR2 detect");
+      #endif
       }
   #endif
 } // END - checkPIRs()
 
+#endif PIR1 or PIR2
