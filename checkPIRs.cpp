c59a5b984a897312c35cbd6a131eb0163f86c760
pauljeffress <paul.jeffress@gmail.com>
2016-10-08 19:23:54 +1100

    Put in code changes so both ON and OFF transitions are now sent.

--- checkPIRs.cpp
+++ checkPIRs.cpp
@@ -14,7 +14,8 @@ void checkPIRs() {
 
   int pir1wait;
   int pir2wait;
-  
+  int newPIR1State, newPIR2State; // used to temp hold the sample taken on the way through here.
+
   #ifdef PIR1
     //PIRdelay; // give the grid time to stabilize for the PIR, otherwise false triggers occur after an RFM send/rcv (might need 2s)
     pir1wait = ((millis() - lastPIR1) < (PIRHOLDOFF * 1000)); // hold-off time for additional PIR1 messages 
@@ -25,31 +26,41 @@ void checkPIRs() {
     pir2wait = ((millis() - lastPIR2) < (PIRHOLDOFF * 1000)); // hold-off time for additional PIR2 messages
   #endif
 
-  #ifdef PIR1
-    curPIR1State = digitalRead(Pir1Input); // read PIR digital pin
-    if (!pir1wait && (curPIR1State == 1)) 
-      { // presence detected!
-      delay(5);
-      lastPIR1 = millis(); // take timestamp
-      send41 = true; // set PIR1 message flag
-      #ifdef DEBUGPJ2
-        Serial.println("PIR1 detect");
-      #endif
-      }
-  #endif
 
-  #ifdef PIR2
-    curPIR2State = digitalRead(Pir2Input); // read PIR digital pin
-    if (!pir2wait && (curPIR2State == 1)) 
-      { // presence detected!
-      delay(5);
-      lastPIR2 = millis(); // take timestamp
-      send43 = true; // set PIR2 message flag
-      #ifdef PJDEBUG2
-        Serial.println("PIR2 detect");
-      #endif
+#ifdef PIR1
+    if (!pir1wait)  // i.e. we are not in the blocking period for this PIR.
+      {
+      newPIR1State = digitalRead(Pir1Input); // read PIR digital pin
+      if (newPIR1State != curPIR1State) 
+        { // PIR state change detected
+        delay(5); // xxxx - do we need this?
+        curPIR1State = newPIR1State;  // copy it into current variable, ready for sending and future comparing.
+        lastPIR1 = millis(); // take new timestamp
+        send41 = true; // set PIR1 message flag
+        #ifdef DEBUGPJ2
+          Serial.println("PIR1 state change");
+        #endif
+        }
       }
-  #endif
+  #endif // PIR1
+
+#ifdef PIR2
+    if (!pir2wait)  // i.e. we are not in the blocking period for this PIR.
+      {
+      newPIR2State = digitalRead(Pir2Input); // read PIR digital pin
+      if (newPIR2State != curPIR2State) 
+        { // PIR state change detected
+        delay(5); // xxxx - do we need this?
+        curPIR2State = newPIR2State;  // copy it into current variable, ready for sending and future comparing.
+        lastPIR2 = millis(); // take new timestamp
+        send43 = true; // set PIR2 message flag
+        #ifdef DEBUGPJ2
+          Serial.println("PIR2 state change");
+        #endif
+        }
+      }
+  #endif // PIR1
+
 } // END - checkPIRs()
 
 #endif PIR1 or PIR2
