94d1bc44920a9fb48015e947d37e7c803f263301
Paul <paul.jeffress@gmail.com>
2021-11-07 18:19:58 +1100

    Point at new MQTT broker at .44

--- PJ-HA-Universal-Node.ino
+++ PJ-HA-Universal-Node.ino
@@ -222,7 +222,7 @@ void mqtt_subs(char* topic, byte* payload, unsigned int length)
     #include <Ethernet.h>
     #include <PubSubClient.h>
     byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0xEF };  // MAC address for ethernet << MUST CHANGE per node!!!!!>>
-    byte mqtt_server[] = { 192, 168, 1, 10};   // MQTT broker address
+    byte mqtt_server[] = { 192, 168, 1, 44};   // MQTT broker address
     byte ip[] = { 192, 168, 1 , 13 };      // Gateway address (if DHCP fails) << MUST CHANGE per node.
     String strPayload;  // used in mqtt_subs() etc, when ingesting a new MQTT message.
     //void mqtt_subs(char* topic, byte* payload, unsigned int length);  // I had to put this prototype here or else I could not compile
@@ -530,5 +530,3 @@ Message mes;
 #endif
 
 // next after here setup() is run and then loop() is run.
-
-
